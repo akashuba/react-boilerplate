67900d2f799f371edaa8c8225f0ada16210ea44c
akashuba <kashuba-aleksandr@mail.ru>
2019-11-03 14:55:53 +0300

    Add image loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
+import  ImageDoggy  from '../assets/doggy.gif'
 
-export const App = () => (
-  <div>
-    <StyledH1>My React App!</StyledH1>
-  </div>
-);
+export const App = () => {
+  console.log(ImageDoggy);
+  
+	return (
+		<div>
+			<StyledH1>My React App!</StyledH1>
+      <DoggyWrapper>
+		  	<Doggy src={ImageDoggy} />
+      </DoggyWrapper>
+		</div>
+	)
+}
 
 const StyledH1 = styled.h1`
-  color: #27aedb;
+	color: #27aedb;
+	text-align: center;
+`
+
+const DoggyWrapper = styled.div`
   text-align: center;
-`;
+`
+
+const Doggy = styled.img`
+	width: 360px;
+`
