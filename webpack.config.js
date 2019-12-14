ad65cc95589eb8bd88cca774b9df25e1267c3210
akashuba <kashuba-aleksandr@mail.ru>
2019-12-14 18:39:46 +0300

    Add mode to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,53 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.join(__dirname, '/dist/'),
-		filename: 'js/index_bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
+module.exports = (env, argv) => {
+
+	return {
+		entry: './src/index.js',
+		output: {
+			path: path.join(__dirname, '/dist/'),
+			filename: 'js/index_bundle.js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+					},
 				},
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				loader: 'file-loader',
-				options: {
-					outputPath: 'images',
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
 				},
-			},
-		],
-	},
-	plugins: [
-		// new HtmlWebpackPlugin({
-		// 	template: './src/index.html',
-		// }),
-		// new BundleAnalyzerPlugin({
-		// 	analyzerMode: 'server',
-		// 	generateStatsFile: true,
-		// 	statsOptions: { source: false },
-		// }),
-	],
+				{
+					test: /\.(png|svg|jpg|gif)$/,
+					loader: 'file-loader',
+					options: {
+						outputPath: 'images',
+					},
+				},
+			],
+		},
+		plugins: plugins(argv.mode),
+	}
+}
+
+const plugins = mode => {
+	if (mode === 'development') {
+		return [
+			new HtmlWebpackPlugin({
+				template: './src/index.html',
+			}),
+		// 	new BundleAnalyzerPlugin({
+		// 		analyzerMode: 'server',
+		// 		generateStatsFile: true,
+		// 		statsOptions: { source: false },
+		// 	}),
+		]
+	}
+
+	return;
 }
