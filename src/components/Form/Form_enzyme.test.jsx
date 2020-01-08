90ef4f63bfa8257efbb97abb5d8c3060825099d6
akashuba <kashuba-aleksandr@mail.ru>
2020-01-08 15:05:33 +0300

    tests: Setup enzyme

--- src/components/Form/Form_enzyme.test.jsx
+++ src/components/Form/Form_enzyme.test.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { shallow } from 'enzyme'
 import { Form } from './Form'
 
-describe('Enzyme', () => {
-	test('Form rendered', () => {
+describe('Form React component test with Enzyme', () => {
+	it('renders without crashing', () => {
+		const wrapper =	shallow(<Form name="Alex" />)
+		console.log(wrapper.debug());
 
+		// expect(wrapper.text()).to.equal('Alex');
+		expect(wrapper.contains(<button type="submit">submit</button>)).toEqual(true);
 	})
 })
