a2c9b60d74852c67cb7d74a570a215c1294e1f1b
akashuba <kashuba-aleksandr@mail.ru>
2020-04-01 10:44:38 +0300

    Start refactoring to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,48 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-import { longListFetch } from '../api/longList'
+import { longListFetch } from '../api/longList';
 
-import ImageDoggy from '../assets/doggy.gif'
+import ImageDoggy from '../assets/doggy.gif';
 
-export class App extends React.Component {
-	componentDidMount() {
+export const App = () => {
+	useEffect(() => {
 		longListFetch().then(async response => {
 			if (response.status === 200) {
-				console.log(await response.json())
+				console.log(await response.json());
 			} else {
-				console.log('Error: ', response.status)
+				console.log('Error: ', response.status);
 			}
-		})
-	}
+		});
+	});
 
-	onButtonClick = () => {
-		console.log('button was clicked')
-	}
+	const onButtonClick = () => {
+		console.log('button was clicked');
+	};
 
-	render() {
-		return (
-			<div>
-				<StyledH1>My React App!</StyledH1>
-				<DoggyWrapper>
-					<Doggy src={ImageDoggy} />
-				</DoggyWrapper>
-				<Button onClick={this.onButtonClick}>Button</Button>
-			</div>
-		)
-	}
-}
+	return (
+		<div>
+			<StyledH1>My React App!</StyledH1>
+			<DoggyWrapper>
+				<Doggy src={ImageDoggy} />
+			</DoggyWrapper>
+			<Button onClick={onButtonClick}>Button</Button>
+		</div>
+	);
+};
 
 const StyledH1 = styled.h1`
 	color: #27aedb;
 	text-align: center;
-`
+`;
 
 const DoggyWrapper = styled.div`
 	text-align: center;
-`
+`;
 
 const Doggy = styled.img`
 	width: 360px;
-`
+`;
 const Button = styled.div`
 	display: flex;
 	align-items: center;
@@ -54,4 +52,4 @@ const Button = styled.div`
 	border: 1px solid black;
 	border-radius: 5px;
 	cursor: pointer;
-`
+`;
